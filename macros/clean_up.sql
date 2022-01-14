41caafa8b9cd058f1131d0da42f51e5434b08f29
Avinash Shekar <avinash.shekar@egen.solutions>
2022-01-14 17:44:35 +0000

    clean_comm

--- macros/clean_up.sql
+++ macros/clean_up.sql
@@ -1,8 +1,18 @@
 {% macro clean_up(schema_name, no_days) %}
 
-SELECT DISTINCT schema_name
-FROM {{schema_name}}.INFORMATION_SCHEMA.schemata
-WHERE schema_name ilike '%S%'
-AND to_date(created) < current_date() - {{no_days}}
+{%- call statement('dist_sch_name', fetch_result=True) -%}
+
+    SELECT DISTINCT schema_name
+    FROM {{schema_name}}.INFORMATION_SCHEMA.schemata
+    WHERE schema_name ilike '%S%'
+    AND to_date(created) < current_date() - {{no_days}}
+
+{%- endcall -%}
+
+{%- set dist_sch = load_result('dist_sch_name')['data'] -%}
+
+{% for sch in dist_sch %}
+DROP SCHEMA schema_name.{{sch}}
+{% endfor %}
 
 {% endmacro %}
