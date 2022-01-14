2042c6bcb0fff3a2a6c30202819de740da63ed00
Avinash Shekar <avinash.shekar@egen.solutions>
2022-01-14 18:06:53 +0000

    clean_comm

--- macros/clean_up.sql
+++ macros/clean_up.sql
@@ -12,7 +12,7 @@
 {%- set dist_sch = load_result('dist_sch_name')['data'] -%}
 
 {% for sch in dist_sch %}
-DROP SCHEMA {{schema_name}}.{{sch[1:-3]}};
+DROP SCHEMA {{schema_name}}.{{sch[1:]}};
 {% endfor %}
 
 {% endmacro %}
